c7d391ffc946c924d924ba293215adf7b012f4c2
sbx_user1051 <sbx_user1051@169.254.122.209>
2022-06-24 23:02:40 +0000

    Fri Jun 24 23:02:40 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -14,6 +14,7 @@ def checkout(skus):
     val = 0
     b = 0
     m = 0
+    q = 0
     for k, v in sorted(dic.items(), reverse=True):
         if k == 'A':
             val += v//5 * 200
@@ -29,7 +30,7 @@ def checkout(skus):
             val += v*35
         elif k == 'J':
             val += v*60
-        elif k == 'C' or 'G':
+        elif k == 'C' or 'G' or 'T':
             val += v*20
         elif k == 'D':
             val += v*15
@@ -39,8 +40,10 @@ def checkout(skus):
             val += v*90
         elif k == 'R':
             val += v*50
-        elif k == 'J':
+        elif k == 'J' or 'Z':
             val += v*50
+        elif k == 'S':
+            val += v*30
         elif k == 'B':
             val += (v-b)//2*45 + (v-b)%2*30
         elif k == 'M':
@@ -53,12 +56,13 @@ def checkout(skus):
             val += v//3*20 + v%3 * 10
         elif k == 'P':
             val += v//5*200 +v%5*50
-        elif k == 'Q’:
-            val += v//3*80 +v%3*30
+        elif k == 'Q':
+            val +=(v-q)//3*80 +(v-q)//3*30
         
     return val
     
         
+
 
 
 

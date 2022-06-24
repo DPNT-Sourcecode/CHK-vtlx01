66103d46e8204fcc59ec6fbd2eb96c6609b50fbf
sbx_user1051 <sbx_user1051@169.254.122.209>
2022-06-24 22:50:40 +0000

    Fri Jun 24 22:50:40 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,7 +5,7 @@
 def checkout(skus):
     dic = {}
     for i in skus:
-        if isupper(i):
+        if not i.isupper():
             return -1
         if i not in dic:
             dic[i] = 1
@@ -18,18 +18,25 @@ def checkout(skus):
             val += v//5 * 200
             v = v%5
             val += v//3 * 130 + v%3 * 50
+        elif k == 'H':
+            val += v//10 * 80
+            v = v%10
+            val += v//5 * 45 + v%5 * 10
         elif k == 'E':
             val += v*40
             b = v//2
-        elif k == 'C':
+        elif k == 'C' or 'G':
             val += v*20
         elif k == 'D':
             val += v*15
         elif k == 'B':
             val += (v-b)//2*45 + (v-b)%2*30
-        else:
+        elif k == 'F':
+            val += v//3*20 + v%3 * 10
+        elif k == 'G':
             val += v//3*20 + v%3 * 10
     return val
     
         
+
 

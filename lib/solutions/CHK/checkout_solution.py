42039aada10e557aaf2f90fb36989b8309633c23
sbx_user1051 <sbx_user1051@169.254.122.209>
2022-06-24 22:14:30 +0000

    Fri Jun 24 22:14:30 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,25 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    lst = ['A', 'B', 'C', 'D']
+    dic = {}
+    for i in skus:
+        if i not in lst:
+            return -1
+        if i not in dic:
+            dic[i] = 1
+        else:
+            dic[i] += 1
+    val = 0
+    for k, v in dic.items():
+        if k == 'A':
+            val += v//3 * 130 + v%3 * 50
+        elif k == 'B':
+            val += v//2 * 45 + v%2 * 30
+        elif k == 'C':
+            val += v*20
+        else:
+            val += v*15
+    return val
+    
+        
